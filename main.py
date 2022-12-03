c9962610655ea0bf4ca0350a6e1042550012fb13
saskokcoc <koreshsashano@gmail.com>
2022-12-03 08:36:39 +0200

    дееее

---
+++ main.py
@@ -0,0 +1,18 @@
+class Тварини(Живі):
+    def дихати(self):
+        pass
+
+    def рухатись(self):
+        pass
+
+    def харчуватись(self):
+        pass
+
+
+class Кошеня(Тварини):
+  def пити_молоко(self):
+    print('П\'ю молочко!')
+  def обідати(self):
+    self.рухатись()
+    print('Я на кухні!')
+    self.пити_молоко()
